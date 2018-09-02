b85082e38419460c5b331514e19766a3c9f7cf77
Phoshino <jonasgiuzio@gmail.com>
2018-09-02 20:08:40 -0300

    primeiro commit

---
+++ apiTeste/src/main/java/br/com/springProject/apiTeste/ApiTesteApplication.java
@@ -0,0 +1,12 @@
+package br.com.springProject.apiTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTesteApplication.class, args);
+	}
+}
